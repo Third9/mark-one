e8b20fe28e8fc96a3119d79b7d0640c372629fc6
dev.third9 <dev.third9@gmail.com>
2017-02-20 11:53:08 +0900

    초기 TextEditor 모듈 생성 및 위치잡기

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -22,6 +22,7 @@ import messages from './messages';
 import { loadRepos } from '../App/actions';
 import { changeUsername } from './actions';
 import { makeSelectUsername } from './selectors';
+import TextEditor from './TextEditor';
 
 export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   /**
@@ -58,6 +59,7 @@ export class HomePage extends React.PureComponent { // eslint-disable-line react
               <FormattedMessage {...messages.startProjectMessage} />
             </p>
           </CenteredSection>
+          <TextEditor />
           <Section>
             <H2>
               <FormattedMessage {...messages.trymeHeader} />
