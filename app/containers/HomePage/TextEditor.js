fd1433e1952d72fbe50a09ae55ff344913221130
dev.third9 <dev.third9@gmail.com>
2017-02-23 18:14:38 +0900

    -. markdown 변환 모듈 변경

--- app/containers/HomePage/TextEditor.js
+++ app/containers/HomePage/TextEditor.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactQuill from 'react-quill';
 
-import commonMark from 'commonmark';
-// import ReactRenderer from 'commonmark-react-renderer';
+import ReactQuill from 'react-quill';
+import marked from 'marked';
 
 import '../../../node_modules/quill/dist/quill.snow.css';
 import './editor.css';
@@ -11,6 +10,17 @@ import './editor.css';
 export default class Editor extends React.Component {
   constructor(props) {
     super(props);
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false,
+    });
+
     this.state = { editorHtml: '', previewHtml: '' };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -18,12 +28,9 @@ export default class Editor extends React.Component {
   }
 
   handleChange(content, delta, source, editor) {
-    // Markdown parser
-    const reader = new commonMark.Parser();
-    const writer = new commonMark.HtmlRenderer({ sourcepos: true });
     const texts = editor.getText();
 
-    console.log('html', content);
+    console.log('html:', content);
     // console.log('delta', delta)
     // console.log('source', source)
     // console.log('editor', editor)
@@ -31,23 +38,11 @@ export default class Editor extends React.Component {
     // console.log('editor.getBounds', editor.getBounds())
     // console.log('editor.getContents', editor.getContents())
     // console.log('editor.getSelection', editor.getSelection())
-    console.log('editor.getText', texts);
+    console.log('editor.getText:', texts);
+    
     // markedown renderer
-    const parsed = reader.parse(texts); // parsed is a 'Node' tree
-    console.log('parsed', parsed);
-
-    // transform parsed if you like...
-    // let walker = parsed.walker();
-    // let event, node;
-
-    // while ((event = walker.next())) {
-    //   node = event.node;
-    //   console.log('node', node)
-
-    // }
-
-    const result = writer.render(parsed); // result is a String
-    console.log('result', result);
+    const result = marked(texts);
+    console.log('result:', result);
 
     this.setState({ editorHtml: content, previewHtml: result });
   }
